90c8a47d82ce168b86483acedfd971802856d954
wangjunkai <125571467@qq.com>
2018-09-18 17:14:03 +0800

    上传图片及生成缩略图并保存

--- handlers/main.py
+++ handlers/main.py
@@ -1,18 +1,42 @@
 import tornado.web
+from utils.photos import get_imgs, make_thumb
 
 
 class IndexHandler(tornado.web.RequestHandler):
     '''主页'''
     def get(self, *args, **kwargs):
-        self.render('index.html')
+        names = get_imgs("uploads")
+        self.render('index.html', imgs=names)
 
 
 class ExploreHandler(tornado.web.RequestHandler):
     '''详情页'''
     def get(self, *args, **kwargs):
-        self.render('explore.html')
+        names = get_imgs("uploads/thumbnails")
+        self.render('explore.html', imgs=names)
 
 
 class PostHandler(tornado.web.RequestHandler):
     def get(self, post_id):
         self.render('post.html', post_id=post_id)
+
+
+class UploadHandler(tornado.web.RequestHandler):
+    """
+    上传图片的保存和展示，以及生成缩略图
+    """
+    def get(self, *args, **kwargs):
+        self.render('upload.html')
+
+    def post(self, *args, **kwargs):
+        # 获取form表单上传的name='newimg'文件的列表
+        # 列表里的元素是格式为{"filename":..., "content_type":..., "body":...}的字典
+        # filename 文件名, body 文件内容
+        img_files = self.request.files.get('newimg', None)
+        for img in img_files:
+            save_to = './static/uploads/{}'.format(img['filename'])
+            with open(save_to, 'wb') as f:
+                f.write(img['body'])
+            # 调用make_thumb方法，生成及保存缩略图
+            make_thumb(save_to)
+            self.write("upload done......")
---
+++ utils/photos.py
@@ -0,0 +1,39 @@
+import os
+import glob
+from PIL import Image
+
+def get_imgs(path):
+    """
+    获取static路径path目录下所有.jpg格式的文件
+    :param path:
+    :return:
+    """
+    # 切换到路径 pwd + static
+    os.chdir("static/")
+    # 获取path目录下的所有jpg文件
+    names = glob.glob("{}/*.jpg".format(path))
+    # 切换到上一级目录
+    os.chdir("..")
+
+    return names
+
+def make_thumb(path):
+    """
+    生成缩略图，并保存到thumbnails目录
+    :param path:
+    :return:
+    """
+    # 获取path路径下，文件的相对路径
+    dirname = os.path.dirname(path)
+    # 获取文件
+    filename = os.path.basename(path)
+    # 分离文件名和文件后缀
+    file, ext = os.path.splitext(filename)
+    # 实例化图片对象
+    im = Image.open(path)
+    size = (128, 128)
+    # 图片缩放尺寸
+    im.thumbnail(size)
+    # 文件保存路径
+    im.save(os.path.join(dirname, 'thumbnails', '{}_{}x{}.jpg'.format(file, *size)), 'JPEG')
+
